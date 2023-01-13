bfa5429746cc519f41d48484ac06046275bbe290
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2023-01-13 19:26:00 +0400

    refactor: add clarifications

--- assets/js/initialStructure.js
+++ assets/js/initialStructure.js
@@ -5,10 +5,9 @@ function mainPageContent() {
         `
         <div class="container">
             <h1>JavaScript Practice</h1>
-            <h2>Modal Window</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quas quos et veritatis aliquid nostrum reprehenderit!
-            </p>
+            <h2>Modal Window 2.0</h2>
+            <p>Fully working modal window</p>
+            <p>Version 2_0</p>
         </div>
         <div class="buttonWrapperDiv">
             <button type="button" class="button" id="button">Click To Open Modal Window To Choose Music</button>
